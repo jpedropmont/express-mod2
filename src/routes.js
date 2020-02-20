fe837838fbe5ea136b84c54164dde53dcff63fe9
jpedropmont <jpedropmont@gmail.com>
2020-02-20 13:09:10 -0300

    Inserting eslint and prettier for the app

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/teste', (req, res) => {
-    return res.json({message: 'Hello World'});
+  return res.json({ message: 'Hello World' });
 });
 
 export default routes;
